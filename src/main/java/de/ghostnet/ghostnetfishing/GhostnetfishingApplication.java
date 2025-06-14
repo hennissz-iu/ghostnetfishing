0e8c5c33014caafee724e544c4628206144706c6
Henrik Schmieder <henrik.schmieder@iu-study.org>
2025-06-14 13:59:42 +0200

    Initial commit

---
+++ src/main/java/de/ghostnet/ghostnetfishing/GhostnetfishingApplication.java
@@ -0,0 +1,13 @@
+package de.ghostnet.ghostnetfishing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostnetfishingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhostnetfishingApplication.class, args);
+	}
+
+}
